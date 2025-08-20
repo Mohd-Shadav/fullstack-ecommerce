6509623a266362a6430f8f766eaac1377346de5f
Mohd Shadav <muhammadshadav166@gmail.com>
2025-08-20 12:57:01 +0530

    commit 20/08/2025 - testing

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
   useEffect(() => {
     async function fetchUserFromCookie() {
   
-      console.log("hlll")
+     
     
         const res = await axios.get('http://localhost:3000/api/users/auth', {
           withCredentials: true
